cb2e8cf7954d90e4e205c7d516d545585d58ec74
Paola Bechalani <148999305+paolabechalani@users.noreply.github.com>
2024-11-29 13:29:54 -0500

    Update mergeSort.h
    updated .h file in order to make sure it runs with the .cpp file

--- mergeSort.h
+++ mergeSort.h
@@ -5,7 +5,12 @@
 #ifndef DSAP3_MERGESORT_H
 #define DSAP3_MERGESORT_H
 
-void merge(int arr[], int left, int mid, int right);
-void mergeSort(int arr[], int left, int right);
+class mergeSort {
+    public:
+        void mergeSortHelper(int arr[], int left, int right);
+
+    private:
+        void merge(int arr[], int left, int mid, int right);
+};
 
 #endif //DSAP3_MERGESORT_H

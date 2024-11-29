262444f86bec79d06815b3793ed095614302b070
Paola Bechalani <148999305+paolabechalani@users.noreply.github.com>
2024-11-29 11:50:56 -0500

    Create mergeSort.h
    mergeSort.h in order to use mergeSort.cpp

---
+++ mergeSort.h
@@ -0,0 +1,11 @@
+//
+// Created by paobe on 11/29/2024.
+//
+
+#ifndef DSAP3_MERGESORT_H
+#define DSAP3_MERGESORT_H
+
+void merge(int arr[], int left, int mid, int right);
+void mergeSort(int arr[], int left, int right);
+
+#endif //DSAP3_MERGESORT_H

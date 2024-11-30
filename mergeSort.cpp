9bef8a3a1eaa2abccd45a060399a2d7f96f703ba
CamilaM1010 <menendezcam443@gmail.com>
2024-11-30 15:19:50 -0500

    Added the mergesort functions to sort by letters

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -51,7 +51,7 @@ void mergeSort::merge(int arr[], int left, int mid, int right) {
 }
 
 void mergeSort::mergeSortHelper(int arr[], int left, int right) {
-    if (left < right) {
+        if (left < right) {
         // m is the point where the array is divided into two subarrays
         int mid = left + (right - left) / 2;
 
@@ -62,5 +62,73 @@ void mergeSort::mergeSortHelper(int arr[], int left, int right) {
         // Merge the sorted halves
         merge(arr, left, mid, right);
     }
+}
+
+void mergeSort::mergeSorter(int arr[], int left, int right) {
+    //Calls the helper function
+    mergeSortHelper(arr, left, right);
+}
+
+void mergeSort::mergeWords(string *arr, int left, int mid, int right) {
+    // Create X ← arr[left..mid] & Y ← arr[mid+1..right]
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    string *X = new string[n1];
+    string *Y = new string[n2];
+
+    for (int i = 0; i < n1; i++)
+        X[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        Y[j] = arr[mid + 1 + j];
+
+    // Merge the arrays X and Y into arr
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (X[i] <= Y[j]) {
+            arr[k] = X[i];
+            i++;
+        } else {
+            arr[k] = Y[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Append remaining elements of X, if any
+    while (i < n1) {
+        arr[k] = X[i];
+        i++;
+        k++;
+    }
+
+    // Append remaining elements of Y, if any
+    while (j < n2) {
+        arr[k] = Y[j];
+        j++;
+        k++;
+    }
+
+    delete[] X;
+    delete[] Y;
+}
+
+void mergeSort::mergeSortHelperWords(string *arr, int left, int right) {
+    if (left < right) {
+        // m is the point where the array is divided into two subarrays
+        int mid = left + (right - left) / 2;
+
+        // Sort the two halves
+        mergeSortHelperWords(arr, left, mid);
+        mergeSortHelperWords(arr, mid + 1, right);
+
+        // Merge the sorted halves
+        mergeWords(arr, left, mid, right);
+    }
+}
+
+void mergeSort::mergeSorterWords(string *arr, int left, int right) {
+    //calls helper function
+    mergeSortHelperWords(arr, left, right);
 }
 
--- mergeSort.h
+++ mergeSort.h
@@ -1,16 +1,22 @@
 //
 // Created by paobe on 11/29/2024.
 //
-
+#include <string>
 #ifndef DSAP3_MERGESORT_H
 #define DSAP3_MERGESORT_H
+using namespace std;
 
 class mergeSort {
     public:
-        void mergeSortHelper(int arr[], int left, int right);
+        void mergeSorter(int arr[], int left, int right);
+        void mergeSorterWords(string arr[], int left, int right);
+
 
     private:
+        void mergeSortHelper(int arr[], int left, int right);
         void merge(int arr[], int left, int mid, int right);
+        void mergeSortHelperWords(string arr[], int left, int right);
+        void mergeWords(string arr[], int left, int mid, int right);
 };
 
 #endif //DSAP3_MERGESORT_H
